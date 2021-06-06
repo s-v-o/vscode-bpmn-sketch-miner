7cba50eb1bc2a8a92199f398af0edd713e8617d3
svo <svo@mailbox.org>
2021-06-06 20:28:59 +0200

    Configurable generator source

---
+++ src/RemoteWebView.ts
@@ -0,0 +1,18 @@
+import * as vscode from "vscode";
+import { WebView } from "./WebView"
+
+export class RemoteWebView extends WebView{
+
+    public getContent(context: vscode.ExtensionContext, content: string, panel:vscode.WebviewPanel) {
+        let url = this.getBpmnSketchMinerUrl(this.encode(content))
+        return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body onload="javascript:window.location = '${url}'">
+</body>
+</html>
+`
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import LZString = require("lz-string");
-import * as path from "path";
-
+import { LocalWebView } from "./LocalWebView"
+import { HybridWebView } from "./HybridWebView"
+import { RemoteWebView } from "./RemoteWebView"
+import { WebView } from "./WebView"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,6 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   let panel: vscode.WebviewPanel | undefined = undefined;
+  let currentSource:string = ""
 
   console.log(
     'Extension "bpmn-sketch-miner" is now active!'
@@ -28,18 +30,36 @@ export function activate(context: vscode.ExtensionContext) {
       let text = vscode.window.activeTextEditor.document.getText() + '\n';
       if (text) {
 
-        if (!panel) {
-          panel = vscode.window.createWebviewPanel(
-            "showBPMNSketchMiner",
-            "show Sketch Miner",
-            vscode.ViewColumn.Two,
-            {
-              enableScripts: true,
-            }
-          );
+        let source = vscode.workspace.getConfiguration('bpmn-sketch-miner').get("generatorSource") as string
+
+        if (panel) {
+          if(source != currentSource){
+            panel.dispose()
+            panel = WebView.createWebPanel()
+            currentSource = source;
+          }
+        }else{
+          panel = WebView.createWebPanel()
+          currentSource = source
+        }
+        let webView: WebView;
+
+        switch (source) {
+          case "local":
+            webView = new LocalWebView()
+            break;
+          case "hybrid":
+            webView = new HybridWebView()
+            break;
+          case "web":
+            webView = new RemoteWebView()
+            break;
+          default:
+            webView = new LocalWebView()
+            break;
         }
 
-        let content = getContent(context, text, panel);
+        let content = webView.getContent(context, text, panel);
         panel.webview.html = content;
 
       }
@@ -50,50 +70,6 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-function getContent(context: vscode.ExtensionContext, content: string, panel:vscode.WebviewPanel) {
-
-  let encoded = LZString.compressToEncodedURIComponent('bpln:v1\n--\n' + content);
-  let bpmnSketchMinerUrl = vscode.Uri.parse("https://www.bpmn-sketch-miner.ai/index.html#" + encoded);
-  let libUri = panel.webview.asWebviewUri(vscode.Uri.file(path.join(context.extensionPath, 'lib/')));
-
-  return `
-<!DOCTYPE html>
-<!-- saved from url=https://www.bpmn-sketch-miner.ai -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>BPMN Sketch Miner</title>
-<meta name="description" content="Sketch process models with the BPMN Business Process Modeling Notation as you describe them in simple natural language">
-<meta name="version" content="1.17.3.3296">
-<meta name="author" content="Cesare Pautasso, Ana Ivanchikj, Ilija Gjorgjiev"> 
-<link rel="stylesheet" href="${libUri}/style.css">
-<link id="favicon" rel="shortcut icon" href="${libUri}/logo.png">
-</head>
-<body data-src="" class="hideNodeFrequency render-done">
-<header><section>
-<h1>BPMN Sketch Miner</h1>
-<nav>
-<div style="flex:1"></div></section><section><nav><span id="button-option-layout">Layout: 
-<a id="button-option-layout-orientation-vertical" href="#" title="Vertical" class="selected">↓</a>
-<a id="button-option-layout-orientation-horizontal" href="#" title="Horizontal">→</a></span>
-<span id="button-option-layout">Zoom: <a id="button-option-layout-zoom-none" href="#" title="1:1">1:1</a>
-<a id="button-option-layout-zoom-fit" href="#" title="Fit" class="selected">Fit</a></span>
-<span id="button-share"><a id="button-share-url-ext" href="${bpmnSketchMinerUrl}" style="">Link</a></span>
-</span>
-</nav></section>
-</header>
-<main>
-<textarea id="logtext" style="display:none;" oninput="live(this.value+'\\n',true);" placeholder="What&#39;s your process like today?">
-    ${content}
-</textarea>
-<div><svg id="restalk" style="">
-</svg>
-</div>
-</main>
-<div style="display: none"><footer><progress id="progress" style="display: none;"></progress><p id="status" style="display: none;">exporting</p></footer>
-<textarea id="explog"></textarea></div>
-<script src="${libUri}/scripts.js"></script> <script src="${libUri}/socket.io/socket.io.js"></script>
-</body></html>
-`
-}
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
